34b347af44b5d9841c88f5f7e4d63f6b84401e54
Gevork <meletjan.kevr94@yandex.ru>
2024-11-13 00:59:44 +0300

    pravka 1go zadaniy

--- task15_1.py
+++ task15_1.py
@@ -1,5 +1,5 @@
 num = int(input("Введите число: "))
 num_list_1 = []
-for digit in range(1, num + 1):
-    num_list_1.append(digit)
+for digit in range(1, num + 1, 2):
+     num_list_1.append(digit)
 print(f"Список из нечётных чисел от одного до N: {num_list_1}")
