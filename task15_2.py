1d05a32ab7018265d42dffd2e1d55c39b605df82
Gevork <meletjan.kevr94@yandex.ru>
2024-11-13 01:09:07 +0300

    second task great

--- task15_2.py
+++ task15_2.py
@@ -1,1 +1,5 @@
-fsd
+participants_list_1 = ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
+first_day = []
+for participants in range(0, len(participants_list_1), 2):
+    first_day.append(participants_list_1[participants])
+print(f"Первый день: {first_day}")
